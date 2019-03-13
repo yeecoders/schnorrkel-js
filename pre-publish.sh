d537c5543749b90f16afc8763dfbb787b95b0acb
Jaco Greeff <jacogr@gmail.com>
2019-03-13 09:26:53 +0100

    Comment unused (for now) asm.js mangling (#7)
    * Comment unused (for now) asm.js mangling
    
    * Update pre-publish.sh

--- pre-publish.sh
+++ pre-publish.sh
@@ -14,13 +14,13 @@ if [ ! -d "binaryen" ]; then
 fi
 
 # build wasm2js as required
-if [ ! -f "binaryen/bin/wasm2js" ]; then
-  echo "*** Building wasm2js"
-  cd binaryen
-  cmake .
-  make wasm2js
-  cd ..
-fi
+# if [ ! -f "binaryen/bin/wasm2js" ]; then
+#   echo "*** Building wasm2js"
+#   cd binaryen
+#   cmake .
+#   make wasm2js
+#   cd ..
+# fi
 
 # build wasm-opt as required
 if [ ! -f "binaryen/bin/wasm-opt" ]; then
@@ -48,8 +48,8 @@ echo "*** Optimising WASM output"
 binaryen/bin/wasm-opt pkg/schnorrkel_js_bg.wasm -Os -o pkg/schnorrkel_js_opt.wasm
 
 # build asmjs version from the input (optimised) WASM
-echo "*** Building asm.js version"
-binaryen/bin/wasm2js --no-validation --output pkg/schnorrkel_js_temp.js pkg/schnorrkel_js_bg.wasm
+# echo "*** Building asm.js version"
+# binaryen/bin/wasm2js --no-validation --output pkg/schnorrkel_js_temp.js pkg/schnorrkel_js_opt.wasm
 
 # convert wasm to base64 structure
 echo "*** Packing WASM into base64"
@@ -75,10 +75,10 @@ sed -i -e 's/"main": "schnorrkel_js\.js"/"main": "index\.js"/g' $PKG
 sed -i -e 's/"types": "schnorrkel_js\.d\.ts"/"types": "index\.d\.ts"/g' $PKG
 
 # cleanup asm
-sed -i -e 's/import {/\/\/ import {/g' $TMP
-sed -i -e 's/function asmFunc/var schnorrkel = require('\''\.\/schnorrkel_js'\''); function asmFunc/g' $TMP
-sed -i -e 's/export const /module\.exports\./g' $TMP
-sed -i -e 's/{abort.*},memasmFunc/schnorrkel, memasmFunc/g' $TMP
+# sed -i -e 's/import {/\/\/ import {/g' $TMP
+# sed -i -e 's/function asmFunc/var schnorrkel = require('\''\.\/schnorrkel_js'\''); function asmFunc/g' $TMP
+# sed -i -e 's/export const /module\.exports\./g' $TMP
+# sed -i -e 's/{abort.*},memasmFunc/schnorrkel, memasmFunc/g' $TMP
 
 # we do not want the __ imports (used by WASM) to clutter up
 sed -i -e 's/var wasm;/const crypto = require('\''crypto'\''); let wasm; const requires = { crypto };/g' $SRC
@@ -148,4 +148,4 @@ rm -rf pkg/*-e
 # pass through uglify to make the actual output smaller
 # echo "*** Optimising asm.js output"
 # yarn run uglifyjs $TMP --compress --mangle --timings --output $ASM
-mv $TMP $ASM
+# mv $TMP $ASM
